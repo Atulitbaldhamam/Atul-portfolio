70f64cc64d4a46957eeaf32859e41769a3d98861
Atulitbaldhamam <146709729+Atulitbaldhamam@users.noreply.github.com>
2026-01-03 00:21:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,18 @@ form.addEventListener("submit", e => {
       submitBtn.innerText = "Send Message";
     });
 });
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+// Toggle Mobile Menu
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(n => n.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+}));
